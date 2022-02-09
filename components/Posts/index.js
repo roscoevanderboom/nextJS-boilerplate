4fb1ba6ff05f80e22fdaa4f7767e2597074e9501
roscoe <roscoevanderboom@protonmail.com>
2022-02-09 09:14:45 +0800

    firebase

--- components/Posts/index.js
+++ components/Posts/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import styles from "../../styles/PostItem.module.css";
 const Post = ({ post }) => {
   return (
-    <Link href="/posts/[id]" as={`/posts/${post.id}`}>
+    <Link href="/posts/[id]" as={`/posts/${post.id}`} passHref>
       <li className={styles.card}>
         <h3>{post.title}</h3>
       </li>
--- firebase/firebase.api.js
+++ firebase/firebase.api.js
@@ -7,13 +7,13 @@ import * as _database from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyACaLu6ZfAgoNnghb8pVXnTQodXfotyiXs",
+  authDomain: "boilerplate-react-native.firebaseapp.com",
+  databaseURL: "https://boilerplate-react-native-default-rtdb.firebaseio.com",
+  projectId: "boilerplate-react-native",
+  storageBucket: "boilerplate-react-native.appspot.com",
+  messagingSenderId: "614988005183",
+  appId: "1:614988005183:web:65a27358e16d3aff5c1d8a"
 };
 
 // Initialize Firebase
