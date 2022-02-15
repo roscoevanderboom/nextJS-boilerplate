a646efa2f9d7d13904f043b317f35418602c8f18
roscoe <roscoevanderboom@protonmail.com>
2022-02-15 13:34:09 +0800

    add styles to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,43 @@
 import type { NextPage } from "next";
+import { Paragraph } from "../components/Typography";
 import Link from "next/link";
 import PublicLayout from "../Layouts/PublicLayout";
+import homePageStyles from "../styles/jss/pages/homePage";
 
 const Home: NextPage = () => {
-
+  const classes = homePageStyles();
   return (
     <PublicLayout>
-      <main>
-      <h1 className="title">Landing page with public layout</h1>
+      <div className={classes.wrapper}>
+        <header>
+          <h1 className="title">Landing page with public layout</h1>
+        </header>
+        <br />
         <Link passHref href="/dashboard/profile">
           <a>Go to Dashboard</a>
         </Link>
-      </main>
+        <br />
+        <main style={{ width: 1000 }}>
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum vero
+            dolores nisi pariatur esse eveniet ea rerum laudantium iusto soluta
+            quasi magnam laborum, eligendi est voluptate quibusdam qui eius
+            porro!
+          </Paragraph>
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum vero
+            dolores nisi pariatur esse eveniet ea rerum laudantium iusto soluta
+            quasi magnam laborum, eligendi est voluptate quibusdam qui eius
+            porro!
+          </Paragraph>
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum vero
+            dolores nisi pariatur esse eveniet ea rerum laudantium iusto soluta
+            quasi magnam laborum, eligendi est voluptate quibusdam qui eius
+            porro!
+          </Paragraph>
+        </main>
+      </div>
     </PublicLayout>
   );
 };
---
+++ styles/jss/pages/homePage.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@mui/styles";
+
+const homePageStyles = makeStyles({
+  wrapper: {
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+});
+
+export default homePageStyles;
